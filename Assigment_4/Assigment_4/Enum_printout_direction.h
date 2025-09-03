b01dfbb061b2798bba4f4d6cfa6e330f4a41db2d
Park Semin <seminpark3317@gmail.com>
2025-09-03 14:31:22 +0900

    Delete Assigment_4/Assigment_4 directory
    uploaded duplication of same folder. deleted the duplication

--- Assigment_4/Assigment_4/Enum_printout_direction.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-enum Enum_PrintoutDirection
-{
-	vertical,
-	horizontal
-};
--- Assigment_4/Assigment_4/SearchResult.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <vector>
-#include <string>
-
-struct SearchResult
-{
-	std::string name;// name
-	std::vector<int> indicator;// record of start and end of the text change color index
-};
